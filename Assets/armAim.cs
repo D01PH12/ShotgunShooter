d3a6daa5f93d663040416decdb27d79f091e0b3f
jabberwock404 <42008427+jabberwock404@users.noreply.github.com>
2022-10-31 12:02:55 -0400

    basic arm movement with camera (buggy)

--- Assets/armAim.cs
+++ Assets/armAim.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class armAim : MonoBehaviour
 {
-
     Vector3 lastMouseCoordinate = Vector3.zero;
+    public bool invertMouse;
+    public float mouseSensitivity;
+    public Camera cam;
 
     // Start is called before the first frame update
     void Start()
@@ -18,19 +21,26 @@ public class armAim : MonoBehaviour
     void Update()
     {
         // First we find out how much it has moved, by comparing it with the stored coordinate.
-        Vector3 mouseDelta = Input.mousePosition - lastMouseCoordinate;
+        //Vector3 mouseDelta = Input.GetAxis("Mouse Y") - lastMouseCoordinate;
+        //curr = cam.transform.rotation.y;
+        //diff = curr - prev;
 
-        if (mouseDelta.y > 0)
+        //this.transform.Rotate(diff, 0, 0);
+
+        //this.transform.Rotate(Input.GetAxis("Mouse Y") * mouseSensitivity * ((invertMouse) ? 1 : -1), 0, 0);
+
+        if (Input.GetAxis("Mouse Y") > 0)
         {
-            this.transform.Rotate(-1, 0, 0);
+            this.transform.Rotate((float)-0.3, 0, 0);
         }
 
-        if (mouseDelta.y < 0)
+        if (Input.GetAxis("Mouse Y") < 0)
         {
-            this.transform.Rotate(1, 0, 0);
+            this.transform.Rotate((float)0.3, 0, 0);
         }
 
         // Then we store our mousePosition so that we can check it again next frame.
         lastMouseCoordinate = Input.mousePosition;
+        //prev = curr;
     }
 }
