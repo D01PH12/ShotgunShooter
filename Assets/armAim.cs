cf0a856bdecd9d3df3953db1c50f3c3a3db365be
davidkollmar6423 <david@homeworkmuffin.com>
2022-10-31 13:41:14 -0400

    mostly fixed arm rotation with cursor

--- Assets/armAim.cs
+++ Assets/armAim.cs
@@ -6,8 +6,6 @@ using System;
 
 public class armAim : MonoBehaviour
 {
-    Vector3 lastMouseCoordinate = Vector3.zero;
-    public bool invertMouse;
     public float mouseSensitivity;
     public Camera cam;
 
@@ -20,27 +18,14 @@ public class armAim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // First we find out how much it has moved, by comparing it with the stored coordinate.
-        //Vector3 mouseDelta = Input.GetAxis("Mouse Y") - lastMouseCoordinate;
-        //curr = cam.transform.rotation.y;
-        //diff = curr - prev;
 
-        //this.transform.Rotate(diff, 0, 0);
+        float Y = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-        //this.transform.Rotate(Input.GetAxis("Mouse Y") * mouseSensitivity * ((invertMouse) ? 1 : -1), 0, 0);
-
-        if (Input.GetAxis("Mouse Y") > 0)
-        {
-            this.transform.Rotate((float)-0.3, 0, 0);
+        if((cam.transform.eulerAngles.x + (-Y) > 80 && cam.transform.eulerAngles.x + (-Y) < 280)) {
+            Y = 0;
         }
 
-        if (Input.GetAxis("Mouse Y") < 0)
-        {
-            this.transform.Rotate((float)0.3, 0, 0);
-        }
+        this.gameObject.transform.Rotate(-Y, 0, 0);
 
-        // Then we store our mousePosition so that we can check it again next frame.
-        lastMouseCoordinate = Input.mousePosition;
-        //prev = curr;
     }
 }
