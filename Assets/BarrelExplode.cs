e038f7464d1077bac73c699bc8c2c97aa08c89f6
davidkollmar6423 <david@homeworkmuffin.com>
2022-11-07 18:55:24 -0500

    attempted to add explosions

--- Assets/BarrelExplode.cs
+++ Assets/BarrelExplode.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class BarrelExplode : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject explode;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter(Collider other) {
+
+        if (other.gameObject.CompareTag("Bullet")) {
+            
+            Instantiate(explode, gameObject.transform);
+            Destroy(gameObject);
+        }    
     }
 }
