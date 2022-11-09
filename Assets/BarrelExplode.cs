0b2615251926920630ffcd9aea941cb340a4651e
davidkollmar6423 <david@homeworkmuffin.com>
2022-11-09 15:44:19 -0500

    added basic tutorial messages and adjusted enemies

--- Assets/BarrelExplode.cs
+++ Assets/BarrelExplode.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BarrelExplode : MonoBehaviour
 {
     public GameObject explode;
+    public float damage;
+    public float radius;
     public float scale;
 
     private void OnTriggerEnter(Collider other) {
@@ -14,7 +16,17 @@ public class BarrelExplode : MonoBehaviour
             
             GameObject boom = Instantiate(explode, gameObject.transform.position, Quaternion.identity);
             boom.transform.localScale = new Vector3(scale, scale, scale);
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
             Destroy(gameObject);
+
+            foreach (var enemy in enemies) {
+                if (Vector3.Distance(enemy.transform.position, gameObject.transform.position) < radius) {
+                    enemy.GetComponent<EnemyAI>().reduceHealth(damage);
+                }
+            }
+            {
+                
+            }
         }    
     }
 }
