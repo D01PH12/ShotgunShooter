dfbf46931de09ba9e24ca9d08f8c2749fdb1f450
davidkollmar6423 <david@homeworkmuffin.com>
2022-11-25 17:34:11 -0500

    added sounds, more scene assets, more scene enemies

--- Assets/TrainCollapse.cs
+++ Assets/TrainCollapse.cs
@@ -8,12 +8,18 @@ public class TrainCollapse : MonoBehaviour
     public GameObject trigger;
     public float triggerRange;
     public GameObject player;
+    public AudioSource slide;
+
+    void Start() {
+        slide = gameObject.GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Vector3.Distance(player.transform.position, trigger.transform.position) < triggerRange) {
             this.gameObject.GetComponent<Rigidbody>().useGravity = true;
+            slide.Play(0);
         }
     }
 }
